f8ad853ae188fcc540a236da7a7fd98112d29eff
Simon Schiele <simon.codingmonkey@gmail.com>
2019-08-22 17:57:52 +0200

    Deply script fixes

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,18 @@
 # will bump revision, commit and upload to storage device
 
 CARD_STORAGE_DEVICE="${1:-$( ls /dev/sd* | tail -n 1 )}"
-CARD_REPL_DEVICE="/dev/ACM1"
+CARD_REPL_DEVICE="/dev/ttyACM1"
 MOUNTPOINT="/media/card10"
 
 echo "Using storage device $CARD_STORAGE_DEVICE"
 
-if [[ ! -e ${CARD_STORAGE_DEVICE} ]] ; then
-	echo "Device not found" >&2
+if [[ -e ${CARD_REPL_DEVICE} ]] ; then
+	echo "Device in normal/non-storage mode" >&2
 	exit 1
 fi
 
-if [[ -e ${CARD_REPL_DEVICE} ]] ; then
-	echo "Device in normal/non-storage mode" >&2
+if [[ ! -e ${CARD_STORAGE_DEVICE} ]] ; then
+	echo "Device not found" >&2
 	exit 1
 fi
 
@@ -34,6 +34,7 @@ if ! sudo cp -fv __init__.py metadata.json ${MOUNTPOINT}/apps/snake/ ; then
 	exit 1
 fi
 sync
+sleep 3
 if ! sudo umount ${MOUNTPOINT} ; then
 	echo "Couldn't unmount $MOUNTPOINT" >&2
 	exit 1
