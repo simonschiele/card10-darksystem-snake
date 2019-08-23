fb4d4ade25c72394b7ebfa6ea9cb906a5695d426
Simon Schiele <simon.codingmonkey@gmail.com>
2019-08-23 17:13:58 +0200

    Make versionbump optional in deploy script

--- deploy.sh
+++ deploy.sh
@@ -23,11 +23,14 @@ if ! sudo mount ${CARD_STORAGE_DEVICE} ${MOUNTPOINT} ; then
 	exit 1
 fi
 
-rev_old=$( grep -o '"revision":[0-9]*' metadata.json | cut -f2 -d':' )
-rev=$(( rev_old + 1 ))
-sed -i "s/revision.*/revision\":${rev}}/g" metadata.json
-echo "Bumping from $rev_old to $rev"
-git commit metadata.json -m "Bump $rev_old -> $rev"
+# give as parameter (after 1) - no time to clean up
+if echo "$@" | grep -iq -e bump -e version ; then
+	rev_old=$( grep -o '"revision":[0-9]*' metadata.json | cut -f2 -d':' )
+	rev=$(( rev_old + 1 ))
+	sed -i "s/revision.*/revision\":${rev}}/g" metadata.json
+	echo "Bumping from $rev_old to $rev"
+	git commit metadata.json -m "Bump $rev_old -> $rev"
+fi
 
 if ! sudo cp -fv __init__.py metadata.json ${MOUNTPOINT}/apps/snake/ ; then
 	echo "Couldn't update app files" >&2
@@ -39,5 +42,7 @@ if ! sudo umount ${MOUNTPOINT} ; then
 	echo "Couldn't unmount $MOUNTPOINT" >&2
 	exit 1
 fi
+
+sudo eject ${CARD_STORAGE_DEVICE} 2>/dev/null >&2
 
 echo "Update $rev_old -> $rev successfull"
